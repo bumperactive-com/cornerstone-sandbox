d01b0eb4a6011293d4819f702e48bdecb835760f
Justin <jschiller@bumperactive.com>
2024-11-12 13:05:49 -0600

    Created sticker-designer template and associated JS file. Moved 'designer' modal outside of React

---
+++ assets/js/theme/custom/designer-product/sticker-designer.js
@@ -0,0 +1,71 @@
+import PageManager from '../../page-manager.js';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ExcalidrawDesigner from '../../../react-components/ExcalidrawDesigner.js';
+import { defaultModal } from '../../global/modal.js';
+import { api } from '@bigcommerce/stencil-utils';
+
+export default class StickerDesigner extends PageManager {
+  constructor(context) {
+    super(context)
+    this.$excalidrawContainer= $('#excalidraw-root')[0];
+  }
+
+  onReady() {
+    const canvasDimensions = this.parseDimensions(this.context.canvas_export_dimensions);
+    this.renderReactComponent(this.$excalidrawContainer, ExcalidrawDesigner, {canvasDimensions: canvasDimensions});
+    this.modal = null;
+    this.bindEvents();
+  }
+
+  parseDimensions(dimensions) {
+    // Validate input format
+    const regex = /^\d+x\d+$/;
+    if (typeof dimensions !== 'string' || !regex.test(dimensions)) {
+      throw new Error("Invalid format. Expected a string in the format 'widthxheight', e.g., '200x300'.");
+    }
+
+    // Split the string by 'x' to separate width and height
+    const [width, height] = dimensions.split('x');
+    
+    // Return as object
+    return {
+      width: parseInt(width, 10),
+      height: parseInt(height, 10)
+    };
+  }
+
+  renderReactComponent(container, ReactComponent, props = {}) {
+    const root = createRoot(container);
+    root.render(<ReactComponent {...props}/>);
+  }
+
+  bindEvents() {
+    this.bindExcalidrawDesignerEvents();
+  }
+
+  bindExcalidrawDesignerEvents() {
+    window.addEventListener('openDesignerReviewModal', (event) => {
+      const excalidrawImgDataUrl = event.detail;
+      this.openDesignerReviewModal(excalidrawImgDataUrl);
+    })
+  }
+
+  openDesignerReviewModal(imgDataUrl) {
+    this.modal = defaultModal();
+    
+    
+    // get modal template code
+    api.getPage('/sticker-designer', {template: 'products/modals/designerReview'}, (err, content) => {
+      if (err) {
+          return this.modal.updateContent(this.context.previewError);
+      }
+      
+      this.modal.updateContent(content);
+      $('.excalidraw-img').attr('src', imgDataUrl);
+    });
+
+    this.modal.open();
+  }
+}
+
