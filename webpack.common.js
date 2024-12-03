c06d5e706cb03310a60a10cca835955e0a373e31
Lukas Schweter <schweterlukas@gmail.com>
2024-12-03 14:32:06 -0600

    removed comment

--- webpack.common.js
+++ webpack.common.js
@@ -39,10 +39,6 @@ module.exports = {
                     },
                 },
             },
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'css-loader'],
-            // },
             {
                 test: require.resolve("jquery"),
                 loader: "expose-loader",
