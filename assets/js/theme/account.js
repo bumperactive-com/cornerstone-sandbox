9b876f25d60fa2992caa3509647d586fb44e2d07
andrewhartfordbac <andrew@bumperactive.com>
2024-12-06 12:11:05 -0600

    Spacing edits

--- assets/js/theme/account.js
+++ assets/js/theme/account.js
@@ -33,7 +33,6 @@ export default class Account extends PageManager {
         const $reorderForm = classifyForm('[data-account-reorder-form]');
         const $invoiceButton = $('[data-print-invoice]');
         const $bigCommerce = window.BigCommerce;
-
         compareProducts(this.context);
 
         // Injected via template
--- assets/js/theme/custom/designer-product/design-gallery.js
+++ assets/js/theme/custom/designer-product/design-gallery.js
@@ -58,5 +58,4 @@ export default class MyDesigns extends PageManager {
         });
     });
   }
-
 }
