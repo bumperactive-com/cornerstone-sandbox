909739480b8c849040c28460326ccd230ab9666f
andrewhartfordbac <andrew@bumperactive.com>
2024-12-06 16:17:34 -0600

    Make title link clickable

--- assets/js/theme/custom/templates/design-gallery-item-template.js
+++ assets/js/theme/custom/templates/design-gallery-item-template.js
@@ -2,7 +2,11 @@ export default function createGalleryItemHtml(design) {
   const categories = design.categories.map(category => category.name).join(", ");
   return `
     <div class="design-item">
-      <p class="design-title"><strong>Title:</strong> ${design.title}</p>
+      <p class="design-title">
+        <a href="/design-gallery/design-detail?id=${design.id}">
+            <strong>Title:</strong> ${design.title}
+        </a>
+      </p>
       <p class="design-categories"><strong>Categories:</strong> ${categories}</p>
       <img src="${design.imageUrl}" alt="${design.title} image" class="design-image">
       <p class="design-actions">
